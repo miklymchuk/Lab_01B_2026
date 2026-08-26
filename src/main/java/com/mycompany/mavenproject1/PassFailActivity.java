c6567df97edd778b791c0df3030bf262445d0641
6312398 <6312398@host.docker.internal>
2026-08-26 14:53:03 -0400

    Implemented PassFailActivity class.

---
+++ src/main/java/com/mycompany/mavenproject1/PassFailActivity.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author 6312398
+ */
+public class PassFailActivity extends GradedActivity {
+    protected double minPassingScore;
+    
+    public PassFailActivity(double minPassingScore) {
+        this.minPassingScore = minPassingScore;
+    }
+    public char getGrade() {
+        if (score > minPassingScore) {
+            return 'P';
+        }
+        return 'F';
+    }
+}
