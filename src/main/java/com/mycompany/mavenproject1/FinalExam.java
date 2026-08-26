65ab7ca6d246243cc94c921d8f587f28e22f5c92
6312398 <6312398@host.docker.internal>
2026-08-26 14:33:34 -0400

    Implemented FinalExam class.

---
+++ src/main/java/com/mycompany/mavenproject1/FinalExam.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author 6312398
+ */
+public class FinalExam extends GradedActivity {
+    int numQuestions;
+    double pointsEach;
+    int numMissed;
+    
+    public FinalExam(int numQuestions, int numMissed) {
+        this.numQuestions = numQuestions;
+        this.numMissed = numMissed;
+        pointsEach = 100/numQuestions;
+    }
+    
+    public double getPointsEach() {
+        return pointsEach;
+    }
+    
+    public int getNumMissed() {
+        return numMissed;
+    }
+}
